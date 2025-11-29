559bccae46532d79c7dc1c6e26b100b45d8014ea
Manojkrishna27 <manojkrishna27@gmail.com>
2025-11-29 20:37:45 +0530

    first sum

---
+++ star1.py
@@ -0,0 +1,29 @@
+
+# *****
+# *****
+# *****
+# *****
+
+for i in range(5):
+    for j in range(5):
+        print("*",end=" ")
+
+# 7x7
+
+for i in range(7):
+    for j in range(7):
+        print("*",end=" ")
+    print()
+
+# 3x6
+for i in  range(3):
+    for j in range(6):
+        print("*",end=" ")
+    print()
+
+# nxn
+n=int(input())
+for i in range(n):
+    for j in range(n):
+        print("*",end=" ")
+    print()
