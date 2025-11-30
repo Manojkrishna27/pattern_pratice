71eaa850d25bc262f14e0ea37920f15ba0af67bb
Manojkrishna27 <manojkrishna2725@gmail.com>
2025-11-30 19:26:38 +0530

    this is half butterfly

---
+++ halfbutterrfly.py
@@ -0,0 +1,5 @@
+# this logic is nothing but merging foward star pattern and reverse pattern
+for i in range(1,5): # this loop for forward
+    print("*"*i)
+for i in range(5,0,-1):# this loop for reverse
+    print("*"*i)
