d78beccaec53b1545a42dabbf52bab38b69f8e1b
Manojkrishna27 <manojkrishna2725@gmail.com>
2025-11-30 19:34:54 +0530

    this is butterfly pattern

--- butterfly.py
+++ butterfly.py
@@ -1,5 +1,7 @@
 n=5
-for i in range(1,n+1):
-    print("*"*i+" "*(2*(n-i))+"*"*i)
-for i in range(n-1,0,-1):
+for i in range(1,n+1): # this is for upper part
+    print("*"*i+" "*(2*(n-i))+"*"*i) 
+for i in range(n-1,0,-1): # this is for lowwer part
     print("*"*i+" "*2*(n-i)+"*"*i)
+
+# understnd the print logic the butterfly is very easy
