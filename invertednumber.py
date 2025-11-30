24e4b74c5005fd885055ce2bd8969a6d820179fc
Manojkrishna27 <manojkrishna2725@gmail.com>
2025-11-30 08:36:31 +0530

    pratice

--- invertednumber.py
+++ invertednumber.py
@@ -1,4 +1,4 @@
-for i in range(5,0,-1): #changeing structure for reverse where 5 to 0 means 5,4,3,2,1 and for reverse -1
+for i in range(5,0,-1): #changing structure for reverse where 5 to 0 means 5,4,3,2,1 and for reverse -1
     for j in range(1,i+1): # inner loop for print the number starts from 1 end with i+1
         print(j,end=" ")
     print()
