e71121ca97c6ff6f8f27d533c7510d2a6b687b8c
Manojkrishna27 <manojkrishna2725@gmail.com>
2025-11-30 19:38:55 +0530

    diamond pattern

--- diamond.py
+++ diamond.py
@@ -1,10 +1,11 @@
 # diamond
-row =5
+row =5 # upper pyramid
 for i in range(1,row+1):
     print(" "*(row-i),end=" ")
-    print("* "*i)
-rows=4
+    print("* "*i) # this space is important
+
+rows=4 # lower pyramid
 for i in range(rows,0,-1):
     print(" "*(rows-i),end=" ")
-    print(" *"*i)
-# test commit
+    print(" *"*i) # this space is important
+# together they form diamond
