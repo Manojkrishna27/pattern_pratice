2e0dd75c6f4527c9906bd0cf2d23456c5fdcce83
Manojkrishna27 <manojkrishna2725@gmail.com>
2026-01-09 17:27:13 +0530

    butterfly

--- butterfly.py
+++ butterfly.py
@@ -4,4 +4,4 @@ for i in range(1,n+1): # this is for upper part
 for i in range(n-1,0,-1): # this is for lowwer part
     print("*"*i+" "*2*(n-i)+"*"*i)
 
-# understnd the print logic the butterfly is very easy
+# understand the print logic the butterfly is very easy
